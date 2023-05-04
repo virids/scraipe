b602d184575e5995d51e8b9dcdf96162dd07ba1c
Marko Mitrović <archamond@gmail.com>
2023-05-04 23:08:20 +0200

    edit

---
+++ server/api/scraper.js
@@ -0,0 +1,48 @@
+const axios = require('axios');
+const cheerio = require('cheerio');
+const cors = require('micro-cors')();
+
+async function scrapeWebpage(url) {
+    try {
+        const response = await axios.get(url);
+        const $ = cheerio.load(response.data);
+        const result = {};
+
+        
+        const headerTags = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'p'];
+
+        $('*').each((index, element) => {
+            const tagName = $(element).prop('tagName').toLowerCase();
+            
+            if (headerTags.includes(tagName)) {
+            const type = tagName.startsWith('h') ? 'header' : 'paragraph';
+            const key = `${type}${index + 1}`;
+            const value = $(element).text().trim();
+            result[key] = value;
+            }
+        });
+
+        return result;
+        } catch (error) {
+        console.error(`Error fetching data from ${url}:`, error.message);
+        }
+}
+
+const handler = async (req, res) => {
+  const { url } = req.query;
+
+  if (!url) {
+    res.status(400).json({ error: 'URL is required' });
+    return;
+  }
+
+  try {
+    const data = await scrapeWebpage(url);
+    res.json(data);
+  } catch (error) {
+    console.error('Error scraping:', error); // Log the error details
+    res.status(500).json({ error: 'Internal server error', details: error.message });
+  }
+};
+
+module.exports = cors(handler);
